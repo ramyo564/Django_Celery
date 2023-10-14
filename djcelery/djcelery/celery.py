ad1adac6a78140f7a01aac56f57266e7d97a4bbb
ramyo564 <yohan032yohan@gmail.com>
2023-10-14 17:19:10 +0900

    chore: configure task routing

--- djcelery/djcelery/celery.py
+++ djcelery/djcelery/celery.py
@@ -5,11 +5,7 @@ from celery import Celery
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djcelery.settings')
 app = Celery("djcelery")
 app.config_from_object("django.conf:settings", namespace="CELERY")
-
-
-@app.task
-def add_numbers():
-    return
-
-
+app.conf.task_routes = {
+    "cworker.tasks.task1": {'queue': 'queue1'}, "cworker.tasks.task2": {'queue': 'queue2'}
+    }
 app.autodiscover_tasks()
