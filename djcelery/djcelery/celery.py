1646c45fb04970a80c66ca6728f24e8f27f7cf9e
ramyo564 <yohan032yohan@gmail.com>
2023-10-14 20:07:52 +0900

    chore: configuring task prioritization(Redis)

--- djcelery/djcelery/celery.py
+++ djcelery/djcelery/celery.py
@@ -5,7 +5,14 @@ from celery import Celery
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djcelery.settings')
 app = Celery("djcelery")
 app.config_from_object("django.conf:settings", namespace="CELERY")
-app.conf.task_routes = {
-    "cworker.tasks.task1": {'queue': 'queue1'}, "cworker.tasks.task2": {'queue': 'queue2'}
-    }
+# app.conf.task_routes = {
+#     "cworker.tasks.task1": {'queue': 'queue1'}, "cworker.tasks.task2": {'queue': 'queue2'}
+#     }
+
+app.conf.broker_transport_options = {
+    'priority_steps': list(range(10)),
+    'sep': ':',
+    'queue_order_startegy': 'priority',
+}
+
 app.autodiscover_tasks()
